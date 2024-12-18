5dd0c816b7c095fdee8b5a2e55391fd06e65ed35
Remi Morisawa <morisawaremi@gmail.com>
2024-12-18 13:19:57 +0900

    Update readme with default squares issue

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # etch-a-sketch
 TOP Etch-a-Sketch Project
+12/18 Issues with the default 256 squares
+  -The default squares have this automatic margin or padding at main start of the first line, offsetting the grid. However, once a number is manually input with the button, the margin is not longer there. 
+  -Unwanted whitespace at main end.
--- js-file.js
+++ js-file.js
@@ -5,7 +5,6 @@ const sketchContainer = document.createElement('div');
 container.appendChild(sketchContainer);
 sketchContainer.id = 'sketchContainer';
 
-
 const btn = document.querySelector('#btn');
 const sq = document.createElement('div'); // Create a box
 sketchContainer.appendChild(sq);
@@ -15,15 +14,6 @@ container.appendChild(sqCount);
 
 let  numOfSqs = 256;
 function makeSq(sketchContainer) {
- 
-
-  /*btn.addEventListener('mouseup',(e) => {
-    numOfSqs = prompt('How many squares?');
-      console.log('hi);');
-      makeSq(sketchContainer, numOfSqs);
-      sqCount.textContent = `${numOfSqs}`;
-  });*/
-
       for (let i = 1; i <= numOfSqs; i++) {
         const sq = document.createElement('div'); // Create a box
         sq.id = `sq${i}`; // Assign a unique ID
@@ -31,8 +21,6 @@ function makeSq(sketchContainer) {
         sketchContainer.appendChild(sq); // Append the box to the container
       }
       sqCount.textContent = `${numOfSqs}`;
-
-
 }
 makeSq(sketchContainer);
 
@@ -61,29 +49,7 @@ btn.addEventListener('click',(e) => {
       sqCount.textContent = `${numOfSqs}`;
     }
   }
-
 });
-
-
-/*btn.addEventListener('click',(e) => {
-  sketchContainer.removeChild(sq);
-  function makeNewNum () {
-    numOfSqs = prompt('How many squares?');
-    console.log('clicked');
-    for (let i = 1; i <= numOfSqs; i++) {
-      const sq = document.createElement('div'); // Create a box
-      sq.id = `sq${i}`; // Assign a unique ID
-      sq.classList.add('sq'); // Add the class 'sq' for styling
-      sketchContainer.appendChild(sq); // Append the box to the container
-    }
-    sqCount.textContent = `${numOfSqs}`;
-
-  }
-    makeNewNum(sketchContainer, numOfSqs);
-    sqCount.textContent = `${numOfSqs}`;
-});*/
-
-
 
 //Drawing
 sketchContainer.addEventListener('mouseover', (e) => {
