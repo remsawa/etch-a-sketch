959bdd397b18d71229a007a6939eb04431a358a1
Remi Morisawa <morisawaremi@gmail.com>
2024-12-18 10:52:19 +0900

    Fix sq clearing function in btn

--- js-file.js
+++ js-file.js
@@ -2,36 +2,28 @@
 const container = document.querySelector('#container');
 
 const sketchContainer = document.createElement('div');
+container.appendChild(sketchContainer);
 sketchContainer.id = 'sketchContainer';
 
 
-const btn = document.querySelector('#btn')
+const btn = document.querySelector('#btn');
+const sq = document.createElement('div'); // Create a box
+sketchContainer.appendChild(sq);
 
 const sqCount = document.createElement('h2');
 container.appendChild(sqCount);
 
-let numOfSqs = 256;
+let  numOfSqs = 256;
 function makeSq(sketchContainer) {
-  btn.addEventListener('mousedown',(e) => {
-    numOfSqs = 0;
-    sqCount.textContent = `${numOfSqs}`;
+ 
 
-  });
-
-  if(down) {
-    console.log('heyo');
-  } else {
-    startDefault();
-  }
-
-  btn.addEventListener('mouseup',(e) => {
+  /*btn.addEventListener('mouseup',(e) => {
     numOfSqs = prompt('How many squares?');
       console.log('hi);');
       makeSq(sketchContainer, numOfSqs);
       sqCount.textContent = `${numOfSqs}`;
-  });
+  });*/
 
-    function startDefault() {
       for (let i = 1; i <= numOfSqs; i++) {
         const sq = document.createElement('div'); // Create a box
         sq.id = `sq${i}`; // Assign a unique ID
@@ -39,14 +31,48 @@ function makeSq(sketchContainer) {
         sketchContainer.appendChild(sq); // Append the box to the container
       }
       sqCount.textContent = `${numOfSqs}`;
-    }
+
 
 }
 makeSq(sketchContainer);
 
+btn.addEventListener('click',(e) => {
+  while (sketchContainer.firstChild) {
+  sketchContainer.removeChild(sketchContainer.firstChild);
+  }
+  numOfSqs = prompt('How many squares?');
 
-container.appendChild(sketchContainer);
-//How about making a function to create all the squares once container is loaded?
+  console.log('Squares Cleared');
+  
+  for (let i = 1; i <= numOfSqs; i++) {
+    const sq = document.createElement('div'); // Create a box
+    sq.id = `sq${i}`; // Assign a unique ID
+    sq.classList.add('sq'); // Add the class 'sq' for styling
+    sketchContainer.appendChild(sq); // Append the box to the container
+  }
+  sqCount.textContent = `${numOfSqs}`;
+
+});
+
+
+/*btn.addEventListener('click',(e) => {
+  sketchContainer.removeChild(sq);
+  function makeNewNum () {
+    numOfSqs = prompt('How many squares?');
+    console.log('clicked');
+    for (let i = 1; i <= numOfSqs; i++) {
+      const sq = document.createElement('div'); // Create a box
+      sq.id = `sq${i}`; // Assign a unique ID
+      sq.classList.add('sq'); // Add the class 'sq' for styling
+      sketchContainer.appendChild(sq); // Append the box to the container
+    }
+    sqCount.textContent = `${numOfSqs}`;
+
+  }
+    makeNewNum(sketchContainer, numOfSqs);
+    sqCount.textContent = `${numOfSqs}`;
+});*/
+
 
 
 //Drawing
