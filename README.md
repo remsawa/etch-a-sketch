58b2704fc43c6b7852175fb9a59ab7ba6e3df25d
Remi Morisawa <morisawaremi@gmail.com>
2025-01-20 10:45:04 +0900

    Fix sq div creator

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 # etch-a-sketch
 TOP Etch-a-Sketch Project
 --
-12/18 
-Issues with the default 256 squares
-  -The default squares have this automatic margin or padding at main start of the first line, offsetting the grid. However, once a number is manually input with the button, the margin is not longer there. 
-  -Unwanted space at main end.
----
-12/19
-Issues with the default 256 squares ==RESOLVED==
-  -Still not sure what was happening with the slight space in the first line, however since I decided to remove sketchContainer's gap for a white canvas effect rather than a grid one, the space seems to have resolved itself.
+1/7
+Decided to start over with the UI and code.
 
-Issue with unwanted space in sketchContainer
-
+1/20
--- js-file.js
+++ js-file.js
@@ -1,32 +1,34 @@
 
-/* OLD CODE
-
 const container = document.querySelector('#container');
+const sketchContainer = document.querySelector('#sketchContainer');
+//const sq = document.createElement('div'); // Create a box
 
-const sketchContainer = document.createElement('div');
-container.appendChild(sketchContainer);
-sketchContainer.id = 'sketchContainer';
 
-const btn = document.querySelector('#btn');
-const sq = document.createElement('div'); // Create a box
+const resetBtn = document.querySelector('#resetBtn');
 sketchContainer.appendChild(sq);
 
-const sqCount = document.createElement('h2');
-container.appendChild(sqCount);
 
-let  numOfSqs = 256;
+const slider = document.querySelector('#slidecontainer');
+
+
+//const sqCount = document.createElement('h2');
+//container.appendChild(sqCount);
+
+const defaultSqs = 256;
+
+//let  numOfSqs = 256;
 function makeSq(sketchContainer) {
-      for (let i = 1; i <= numOfSqs; i++) {
-        const sq = document.createElement('div'); // Create a box
-        sq.id = `sq${i}`; // Assign a unique ID
-        sq.classList.add('sq'); // Add the class 'sq' for styling
-        sketchContainer.appendChild(sq); // Append the box to the container
-      }
-      sqCount.textContent = `${numOfSqs}`;
+  for (let i = 1; i <= defaultSqs; i++) {
+    const sq = document.createElement('div'); // Create a box
+    sq.id = `sq${i}`; // Assign a unique ID
+    sq.classList.add('sq'); // Add the class 'sq' for styling
+    sketchContainer.appendChild(sq); // Append the box to the container
+  }
+  //sqCount.textContent = `${numOfSqs}`;
 }
 makeSq(sketchContainer);
 
-btn.addEventListener('click',(e) => {
+resetBtn.addEventListener('click',(e) => {
   numOfSqs = prompt('How many squares?');
 
   if(numOfSqs <= 500) {
@@ -69,7 +71,4 @@ btn.addEventListener('click',(e) => {
 sketchContainer.addEventListener('mouseover', (e) => {
   e.target.style.backgroundColor = 'black';
 });
-
-*/
-
 
